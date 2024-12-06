0935153bc533f3ea14d1409c510b6a686f4ef1d8
mirzayevt23 <mirzayevt23@gmail.com>
2024-12-06 20:40:09 +0500

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,30 @@
-<script>
+<template>
+  <div class="text-black">
+    Hirepleropvme
+  </div>
+</template>
 
+
+
+<script>
+import {createRouter, createWebHistory} from "vue-router";
+import {ref} from  'vue'
+
+// const router = createRouter(createWebHistory())
+
+const count = ref(4)
+
+
+const a = 10
+
+function increment(){
+  const returnResult =  count.value + a
+  return returnResult
+
+}
+const result = increment()
+
+console.log(result)
 </script>
 
 
-<template>
-  <div class="">
-    Hi
-  </div>
-</template>
